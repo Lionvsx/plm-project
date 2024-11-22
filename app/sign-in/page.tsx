4025c9e3848c401b9a9ac5d09801c990a36a81b1
Leonard Roussard <lroussard@gmail.com>
2024-11-22 18:29:35 +0100

    fix: sign in redirect

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,16 +1,12 @@
 import { LoginForm } from "@/components/login-form";
+import { redirect } from "next/navigation";
 import { getServerSession } from "@/controllers/auth";
 
 export default async function SignInPage() {
   const session = await getServerSession()
 
   if (session?.user) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-4xl font-bold mb-4">Welcome back!</h1>
-        <p className="mb-4">You are already signed in as {session.user.name}</p>
-      </div>
-    );
+    return redirect("/dashboard");
   }
 
   return (
